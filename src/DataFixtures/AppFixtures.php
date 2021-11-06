d4d7cda7f50f76159c1bebc53e6e0dd8ccbff364
Maxime <elessamaxime@icloud.com>
2021-11-06 01:47:02 +0100

    [feat + style] : orm-fixture + revue de code

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+
+        foreach ($this->getUserData() as  [$name, $email, $number, $sex, $location, $password, $role]) {
+            
+            $user = new User();
+
+            $user->setUsername($name)
+                ->setEmail($email)
+                ->setNumber($number)
+                ->setSex($sex)
+                ->setLocation($location)
+                ->setPassword($this->passwordHasher->hashPassword($user, $password))
+                ->setRoles($role);
+
+            $manager->persist($user);
+        }
+
+
+        $manager->flush();
+    }
+
+    private function getUserData(): array
+    {
+        return [
+            ['Maxime_admin', 'maxime@admin.com', '659019493', 'M', 'Abang', '1234', ['ROLE_ADMIN']],
+            ['Maxime_user', 'maxime@user.com', '659019493', 'M', 'Awae', '1234', ['ROLE_USER']]
+        ];
+    }
+}
