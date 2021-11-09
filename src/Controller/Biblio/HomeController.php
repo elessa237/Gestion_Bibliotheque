855cb123a5f84867f53ebc70c6c1060b988f27cf
Maxime <elessamaxime@icloud.com>
2021-11-09 21:15:00 +0100

    [feat] : mise en place de cache d'image

--- src/Controller/Biblio/HomeController.php
+++ src/Controller/Biblio/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Biblio;
 
+use App\Repository\DocumentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="biblio_home")
      */
-    public function index(): Response
+    public function index(DocumentRepository $documents): Response
     {
-        return $this->render('biblio/home/index.html.twig');
+        return $this->render('biblio/home/index.html.twig',[
+            'documents' => $documents->findAll(),
+        ]);
     }
 }
