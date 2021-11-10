f67e8abc9d89aaf33e72a745afd497871866dccb
Maxime <elessamaxime@icloud.com>
2021-11-10 09:30:12 +0100

    [feat] : pagination des documents

--- src/Controller/Biblio/HomeController.php
+++ src/Controller/Biblio/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Biblio;
 
 use App\Repository\DocumentRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +14,18 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="biblio_home")
      */
-    public function index(DocumentRepository $documents): Response
-    {
-        return $this->render('biblio/home/index.html.twig',[
-            'documents' => $documents->findAll(),
+    public function index(
+        DocumentRepository $document,
+        Request $request,
+        PaginatorInterface $pagination
+    ): Response {
+        $documents = $pagination->paginate(
+            $document->findAll(),
+            $request->query->getInt('page', 1),
+            3
+        );
+        return $this->render('biblio/home/index.html.twig', [
+            'documents' => $documents,
         ]);
     }
 }
